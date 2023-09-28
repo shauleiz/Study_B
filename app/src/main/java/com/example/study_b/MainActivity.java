536f8516887b7deac9c34755ead46d7cc8152ae9
Shauleiz <shaul.eiz@gmail.com>
2023-09-28 17:35:26 +0300

    Added +1 button - Increments counter. Display added Counter reset with every change

--- app/src/main/java/com/example/study_b/MainActivity.java
+++ app/src/main/java/com/example/study_b/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.study_b;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+// MainActivity is the default visual page of the application
 public class MainActivity extends AppCompatActivity {
 
+    int counter =0; // Counter
+    TextView CounterView; // Window to show counter
+
+
+    // OnCreate is called everytime the main activity is created
+    // It initializes the UI
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Call the parent class 'OnCreate' method then display the activity as defined in the XML file
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Increment-by-one button
+        // Get it from the XML file then define what it does when clicked
+        Button Inc_Button = (Button) findViewById(R.id.Plus1_button);
+        Inc_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Increment();
+            }
+        });
+
+        CounterView = (TextView) findViewById(R.id.textViewCounter);
+        CounterView.setText(String.valueOf(counter));
     }
+
+
+    // function that increments counter by 1
+    public void Increment()
+    {
+        counter++;
+        CounterView.setText(String.valueOf(counter) );
+    }
+
+
 }
+
+
+
